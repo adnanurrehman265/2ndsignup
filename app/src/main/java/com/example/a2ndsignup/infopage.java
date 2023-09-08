94910588522c6cd4175c42bff6705d115214a526
adnanurrehman265 <adnanurrehman265@gmail.com>
2023-09-08 10:40:34 +0500

    application

---
+++ app/src/main/java/com/example/a2ndsignup/infopage.java
@@ -0,0 +1,14 @@
+package com.example.a2ndsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class infopage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infopage);
+    }
+}
